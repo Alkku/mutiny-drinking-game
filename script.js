253478df84ac648e52618bfda48fb5e31d602ae1
alkku92 <vca92@hotmail.com>
2020-09-03 22:38:00 +0300

    Ändringar samt game loop klar. Nu bara o bygga på features.

--- script.js
+++ script.js
@@ -1,39 +1,55 @@
 // MUTINY DRINKING GAME JS
 
 //Variable list
-var card_is_flipped = false; //initial state: kortet är inte flippat
-const cards = document.querySelectorAll('.drinking-card');
-var chosenCard = document.querySelector('p');
-
-//Skapar lista på kortmöjligheter -> Hur ska vi gö de här? Varje string en mening? Eller kan man gö de här på annat sätt?
-let cardList = ['SkipCard: Skippar din tur till nästa spelare asd asd as da asd asd',
-                'CounterCard: Counterar kaptenens attack ipsum lorem ipsum lorem',
-                'ScoreCard: Ta 5 Huikkan',
-                'ScoreCard2: Ta 6 Huikkan ASD ASD ASD ASD ASDAS DAS D',
-                'ScoreCard3: Ta 3 huikkan Test test etstsetset',
-                'ScoreCard4: Ta en yoloswägger i bägger yolo bolo molo test test',
-                'ScoreCard5: OTTO YOLO SWAG TESTAR ATT SKRIVA I KORTEN YOLO OMG.',
-                'ScoreCard6',
-                'ScoreCard7',
-                'ScoreCard8']
+const card = document.querySelector(".drinking-card");
+const cardfront = document.querySelector(".front-face");
+const cardback = document.querySelector(".back-face");
+var randomcard = document.getElementById("random-card");
+//var captain_score = 0;
+//var crew_total_score = 0;
 
 
-function flipCard() {
-  if (!card_is_flipped) { //If-statement för att hindra att man kan trycka två gånger efter varandra på kortet
-      this.classList.toggle('flip');
-      chosenCard.innerHTML=cardList[Math.floor(Math.random()*cardList.length)]; //Tar random värde från array cardList
-      card_is_flipped = true; //VET DU OTTO HUR MAN FÅR DEN HÄR VARIABELN ATT BYTAS TILL TRUE UTANFÖR FUNKTIONEN?
-  }
+function selectRandomCard() {
+    const cardList = ['SKIP CARD !<br/>Player gets to skip their turn when used.',
+                      'MUTINY !</br>The player that gets this card will.. TBD',
+                      'WATERFALL !<br/></br>Everybody finishes their drink one at a time. Deducts points from random players.',
+                      'COUNTER CARD !</br>Player gets to counter a captain\'s card.',
+                      'The crew member takes 3 sips.'];
+    const arrayNumber = [Math.floor(Math.random()*cardList.length)]
+    randomcard.innerHTML = cardList[arrayNumber];
+    console.log(arrayNumber);
 }
 
 
-/* ALKKU JOBBAR PÅ DEN HÄR DELEN -> Om kortet är scorecard, så discardas den, annars sparas den */
-function discardCard() {
-  if (card_is_flipped) {
-    this.classList.toggle('discard'); //Animation
-    var card_is_flipped = false;
-  }
+
+
+
+
+
+function newCard() {
+  setTimeout(function(){
+    card.classList.toggle('newCard');
+  }, 1500);
+  setTimeout(selectRandomCard, 1500);
+}
+
+function flipCardIfFront() {
+  card.classList.toggle('newCard');
+  card.classList.toggle('flip');
+}
+
+function discardCardIfBack() {
+  card.classList.toggle('flip');
+  card.classList.toggle('discard');
+  newCard();
+  //card.classList.toggle('discard');
 }
 
 
-cards.forEach(card => card.addEventListener('click', flipCard)); //Event listener för att clicka me musen på kortet
+function main() {   // Game loop
+        newCard();
+        cardfront.addEventListener('click', flipCardIfFront);
+        cardback.addEventListener('click', discardCardIfBack);
+}
+
+main();
