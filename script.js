b2dbec712f91c5412c6d937e9f8d9110c925125d
alkku92 <vca92@hotmail.com>
2020-09-05 00:31:00 +0300

    Ny fungerande loop, med början och slutscreens.

--- script.js
+++ script.js
@@ -2,37 +2,78 @@
 
 //--------------------- GAME LOGIC STARTS HERE -------------------------
 
+const nameSplash = document.querySelector(".crew-member-names");
+const endSplash = document.querySelector(".end-game-screen");
+const startFirstGameButton = document.getElementById("start-game-button");
+const startSecondGameButton = document.getElementById("start-game-button");
+const restartGameButton = document.getElementById("restart-game-button");
+var playerNameArray = [];
+
+pushData = function() {
+    var inputText = document.getElementById("crew-member-name").value;
+    playerNameArray.push(inputText);
+    console.log(playerNameArray);
+    document.getElementById("crew-member-name").value = '';
+}
+
+removeLastSplash = function() {
+    nameSplash.classList.toggle('removeSplash');
+}
+
+removeEndSplash = function() {
+    endSplash.classList.toggle('addSplash');
+}
+
+startFirstGameButton.addEventListener('click', () => {
+  removeLastSplash();
+  gameStart();
+  gameLoop();
+});
+
+restartGameButton.addEventListener('click', () => {
+  removeEndSplash();
+  gameStart();
+  gameLoop();
+});
+
+
 //Variable list
-const card = document.querySelector(".drinking-card");
+let card = document.querySelector(".drinking-card");
 const cardfront = document.querySelector(".front-face");
 const cardback = document.querySelector(".back-face");
 const crewScore_span = document.getElementById("crew-score"); //Använd dessa för att uppdatera score i html
 const captScore_span = document.getElementById("capt-score");
 
 
+//selectNumberFromArray = function() {
+//  const arrayNumber = Math.floor(Math.random()*5) //Antalet kort är 5
+//  return arrayNumber;
+//}
 
-selectNumberFromArray = function() {
-  const arrayNumber = Math.floor(Math.random()*5) //Antalet kort är 5
-  return arrayNumber;
-}
 
 addCrewScore = function() {
   this.crewScore += 5000;
   crewScore_span.innerHTML = this.crewScore;
+  console.log(this.crewScore);
   return this.crewScore;
 }
 
-addCaptScore = function() {}
+addCaptScore = function() {
+this.captScore += 5000;
+captScore_span.innerHTML = this.captScore;
+console.log(this.captScore);
+return this.captScore;
+}
+
 
 newCard = function() {
   setTimeout(function(){card.classList.toggle('newCard');}, 2000);
-  setTimeout(selectNumberFromArray, 2000);
+  //setTimeout(selectNumberFromArray, 2000);
 }
 
 flipCardIfFront = function() {
   card.classList.toggle('newCard');
   card.classList.toggle('flip');
-  return addCrewScore();
 }
 
 discardCardIfBack = function() {
@@ -40,9 +81,17 @@ discardCardIfBack = function() {
   card.classList.toggle('discard');
   setTimeout(function(){card.classList.toggle('discard');}, 2000);
   newCard();
+  addCrewScore(crewScore);
+  addCaptScore(captScore);
+  console.log(crewScore + captScore);
+  if (crewScore > 10000) {
+    gameEnd();
+  }
 }
 
 gameStart = function(){
+  crewScore_span.innerHTML = 0;
+  captScore_span.innerHTML = 0;
   this.isMutinyRound = false;
   this.crewScore = 0;
   this.captScore = 0;
@@ -50,15 +99,13 @@ gameStart = function(){
   this.playerNameArray = ['Asta', 'Markus', 'Saida'];
 }
 
+gameEnd = function(){
+  setTimeout(function(){endSplash.classList.toggle('addSplash');}, 1500);
+  card.classList.toggle('newCard');
+}
+
 gameLoop = function() {  //Game loop
-  gameStart();
-        if (this.crewScore < this.scoreToWin){
           newCard();
           cardfront.addEventListener('click', flipCardIfFront);
           cardback.addEventListener('click', discardCardIfBack);
-          console.log(this.crewScore);
-        }
 }
-
-
-gameLoop();
